08b0a8da4c59fcc038713ddc4f74ac83a577f1ce
MatusPilnan <m.pilnan@gmail.com>
2020-05-20 22:45:52 +0200

    add security to team creation and add API call to get team members

--- digital_relay_server/api/api.py
+++ digital_relay_server/api/api.py
@@ -7,7 +7,7 @@ from mongoengine import DoesNotExist, NotUniqueError
 
 from digital_relay_server.api.models import models
 from digital_relay_server.api.security import authorizations
-from digital_relay_server.db import Team
+from digital_relay_server.db import Team, User
 
 blueprint = Blueprint('api', __name__)
 api = Api(app=blueprint, title="DXC RUN 4U API", doc="/documentation")
@@ -35,6 +35,7 @@ registration_response = ns_auth.model('Registration response', {'meta': fields.N
                                                                 'response': fields.Nested(registration_response_body)})
 team = ns_teams.model('Team model', models['team_model'])
 team_list = ns_teams.model('Teams list', {'teams': fields.List(fields.Nested(team))})
+user_list = ns_teams.model('User list', {'users': fields.List(fields.Nested(user))})
 
 
 @ns_auth.route('')
@@ -71,24 +72,26 @@ class HelloWorld(Resource):
 
 @ns_teams.route('')
 class Teams(Resource):
-    @ns_teams.expect(team)
+    @jwt_required()
+    @ns_teams.doc(security=authorizations)
+    @ns_teams.expect(auth_header_parser, team)
     @ns_teams.response(code=200, description='Team creation successful', model=team)
     @ns_teams.response(code=400, description='Bad request', model=error)
     @ns_teams.response(code=409, description='Team already exists', model=error)
     def post(self):
-        """Creates a new team"""
+        """Create a new team"""
         data = request.json
         try:
-            newTeam = Team(name=data['name'], members=data['members'])
+            new_team = Team(name=data['name'], members=data['members'] + [current_identity.email])
         except KeyError as e:
             return marshal({"description": f'{e.args[0]} is a required parameter',
                             "error": 'Missing required parameter',
                             "status_code": 400}, error), 400
         try:
-            response = newTeam.save()
+            response = new_team.save()
             return marshal(response, team), 200
         except NotUniqueError:
-            return marshal({"description": f'Team named {newTeam.name} already exists',
+            return marshal({"description": f'Team named {new_team.name} already exists',
                             "error": 'Invalid team name',
                             "status_code": 409}, error), 409
 
@@ -98,7 +101,7 @@ class Teams(Resource):
     @ns_teams.response(code=200, description='OK', model=team_list)
     @ns_teams.response(code=401, description='Unauthorized', model=error)
     def get(self):
-        """Retrieves all teams that the current user belongs to"""
+        """Retrieve all teams that the current user belongs to"""
         teams = Team.objects(members=current_identity.email)
         return marshal({'teams': teams}, team_list), 200
 
@@ -114,6 +117,35 @@ class TeamResource(Resource):
         try:
             response = Team.objects.get(id=ObjectId(team_id))
             return marshal(response, team), 200
+        except InvalidId:
+            return marshal({"description": f'{team_id} is not a valid ObjectID',
+                            "error": 'Invalid ID',
+                            "status_code": 400}, error), 400
+        except DoesNotExist:
+            return marshal({"description": f'Team with team ID {team_id} does not exist',
+                            "error": 'Not found',
+                            "status_code": 404}, error), 404
+
+
+# noinspection PyUnresolvedReferences
+@ns_teams.route('/<team_id>/users')
+class TeamMembers(Resource):
+    @ns_teams.response(code=200, description='OK', model=team)
+    @ns_teams.response(code=400, description='Invalid ID', model=error)
+    @ns_teams.response(code=404, description='Team not found', model=error)
+    def get(self, team_id):
+        """Retrieve team members as user objects"""
+        try:
+            team = Team.objects.get(id=ObjectId(team_id))
+            emails = team.members
+            users = list(User.objects(email__in=emails))
+            for user in users:
+                if user.email in emails:
+                    emails.remove(user.email)
+
+            for email in emails:
+                users.append(User(id='null', name='null', email=email))
+            return marshal({'users': users}, user_list), 200
         except InvalidId:
             return marshal({"description": f'{team_id} is not a valid ObjectID',
                             "error": 'Invalid ID',
